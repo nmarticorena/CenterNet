4255145f7d64b394f134923908dbf2307f302fb2
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2019-11-25 23:56:50 -0300

    add script res2dcn 50

---
+++ experiments/ctdet_coco_res2dcn50.sh
@@ -0,0 +1,10 @@
+cd src
+# train
+python main.py ctdet --exp_id coco_res2dcn101 --arch res2dcn_101 --batch_size 1 --master_batch 1 --lr 3.75e-4 --gpus 0 --num_workers 16
+# test
+#python test.py ctdet --exp_id coco_res2dcn101 --keep_res --resume
+# flip test
+#python test.py ctdet --exp_id coco_res2dcn101 --keep_res --resume --flip_test
+# multi scale test
+#python test.py ctdet --exp_id coco_res2dcn101 --keep_res --resume --flip_test --test_scales 0.5,0.75,1,1.25,1.5
+#cd ..
