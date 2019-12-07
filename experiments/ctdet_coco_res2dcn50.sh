892458250f884b0187281ffd5578b8e2934cddce
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2019-12-07 18:33:44 -0300

    Res2Net lista para entrenar

--- experiments/ctdet_coco_res2dcn50.sh
+++ experiments/ctdet_coco_res2dcn50.sh
@@ -1,10 +1,10 @@
 cd src
 # train
-python main.py ctdet --exp_id coco_res2dcn50 --arch res2dcn_50 --batch_size 32 --master_batch 32 --lr 3.75e-4
+python main.py ctdet --exp_id coco_res2dcn50 --arch res2dcn_50 --batch_size 1 --master_batch 1 --lr 3.75e-4
 # test
-#python test.py ctdet --exp_id coco_res2dcn101 --keep_res --resume
+python test.py ctdet --exp_id coco_res2dcn101 --keep_res --resume
 # flip test
-#python test.py ctdet --exp_id coco_res2dcn101 --keep_res --resume --flip_test
+python test.py ctdet --exp_id coco_res2dcn101 --keep_res --resume --flip_test
 # multi scale test
-#python test.py ctdet --exp_id coco_res2dcn101 --keep_res --resume --flip_test --test_scales 0.5,0.75,1,1.25,1.5
+python test.py ctdet --exp_id coco_res2dcn101 --keep_res --resume --flip_test --test_scales 0.5,0.75,1,1.25,1.5
 #cd ..
