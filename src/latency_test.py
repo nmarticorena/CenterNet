4083fb44ef12df4016eb35b1bcd9b9fde3db59c3
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2020-02-15 15:59:56 -0300

    Add latency test file

---
+++ src/latency_test.py
@@ -0,0 +1,63 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import _init_paths
+
+import os
+
+import torch
+import torch.utils.data
+from opts import opts
+from models.model import create_model, load_model, save_model
+from models.data_parallel import DataParallel
+from logger import Logger
+from datasets.dataset_factory import get_dataset
+from trains.train_factory import train_factory
+import time
+import numpy as np
+
+
+def computeTime(model, device='cuda'):
+    inputs = torch.randn(10, 3, 224, 224)
+    if device == 'cuda':
+        model = model.cuda()
+        inputs = inputs.cuda()
+
+    model.eval()
+
+    i = 0
+    time_spent = []
+    while i < 100:
+        start_time = time.time()
+        with torch.no_grad():
+            _ = model(inputs)
+
+        if device == 'cuda':
+            torch.cuda.synchronize()  # wait for cuda to finish (cuda is asynchronous!)
+        if i != 0:
+            time_spent.append(time.time() - start_time)
+        i += 1
+    print('Avg execution time (ms): {:.3f}'.format(np.mean(time_spent)))
+
+
+
+def main(opt):
+  torch.manual_seed(opt.seed)
+  torch.backends.cudnn.benchmark = not opt.not_cuda_benchmark and not opt.test
+  Dataset = get_dataset(opt.dataset, opt.task)
+  opt = opts().update_dataset_info_and_set_heads(opt, Dataset)
+  print(opt)
+
+  logger = Logger(opt)
+
+  os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpus_str
+  opt.device = torch.device('cuda' if opt.gpus[0] >= 0 else 'cpu')
+  
+  print('Creating model...')
+  model = create_model(opt.arch, opt.heads, opt.head_conv)
+  computeTime(model)
+
+if __name__ == '__main__':
+  opt = opts().parse()
+  main(opt)
