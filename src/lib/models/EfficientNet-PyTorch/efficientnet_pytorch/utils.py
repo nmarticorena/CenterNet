3f07f21383bc0ff08e4cd679c35f742d89f8bb56
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2020-03-02 01:46:51 -0300

    modify efficient net in order to run in python 2.7

--- src/lib/models/EfficientNet-PyTorch/efficientnet_pytorch/utils.py
+++ src/lib/models/EfficientNet-PyTorch/efficientnet_pytorch/utils.py
@@ -123,7 +123,7 @@ class Conv2dStaticSamePadding(nn.Conv2d):
     """ 2D Convolutions like TensorFlow, for a fixed image size"""
 
     def __init__(self, in_channels, out_channels, kernel_size, image_size=None, **kwargs):
-        super().__init__(in_channels, out_channels, kernel_size, **kwargs)
+        super(Conv2dStaticSamePadding,self).__init__(in_channels, out_channels, kernel_size, **kwargs)
         self.stride = self.stride if len(self.stride) == 2 else [self.stride[0]] * 2
 
         # Calculate padding based on image size and save it
@@ -132,8 +132,8 @@ class Conv2dStaticSamePadding(nn.Conv2d):
         kh, kw = self.weight.size()[-2:]
         sh, sw = self.stride
         oh, ow = math.ceil(ih / sh), math.ceil(iw / sw)
-        pad_h = max((oh - 1) * self.stride[0] + (kh - 1) * self.dilation[0] + 1 - ih, 0)
-        pad_w = max((ow - 1) * self.stride[1] + (kw - 1) * self.dilation[1] + 1 - iw, 0)
+        pad_h = int(max((oh - 1) * self.stride[0] + (kh - 1) * self.dilation[0] + 1 - ih, 0))
+        pad_w = int(max((ow - 1) * self.stride[1] + (kw - 1) * self.dilation[1] + 1 - iw, 0))
         if pad_h > 0 or pad_w > 0:
             self.static_padding = nn.ZeroPad2d((pad_w // 2, pad_w - pad_w // 2, pad_h // 2, pad_h - pad_h // 2))
         else:
