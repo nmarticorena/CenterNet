a69829d63f4cb91e01f31db193a9417937a5a19a
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2020-03-02 18:36:46 -0300

    Fix error, working version

--- src/lib/models/networks/efficent_net.py
+++ src/lib/models/networks/efficent_net.py
@@ -13,8 +13,8 @@ import torch.utils.model_zoo as model_zoo
 
 import numpy as np
 from  efficientnet_pytorch import EfficientNet
-
-from torchsummary import summary
+from .DCNv2.dcn_v2 import DCN
+BN_MOMENTUM = 0.1
 
 BatchNorm = nn.BatchNorm2d
 
@@ -42,21 +42,30 @@ def fill_fc_weights(layers):
 
 class EfficientNetSeg(nn.Module):
     def __init__(self, base_name, heads,
-                 pretrained=True, head_conv=26):
+                 pretrained=True, head_conv=256):
         super(EfficientNetSeg, self).__init__()
         self.heads = heads
+        print (heads)
+        self.inplanes = 1536
+        self.deconv_with_bias = False
 
         print('efficientnet-b{}'.format(base_name))
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64, momentum=BN_MOMENTUM)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.model = EfficientNet.from_pretrained('efficientnet-b{}'.format(base_name))
-        self.model.to('cuda')
-        summary(self.model, (3, 224, 224), device="cuda")
-
-
+        self.deconv_layers = self._make_deconv_layer(
+            3,
+            [768, 384, 64],
+            [4,4, 4],
+        )
         for head in self.heads:
             classes = self.heads[head]
             if head_conv > 0:
                 fc = nn.Sequential(
-                    nn.Conv2d(1536, head_conv,
+                    nn.Conv2d(64, head_conv,
                               kernel_size=3, padding=1, bias=True),
                     nn.ReLU(inplace=True),
                     nn.Conv2d(head_conv, classes,
@@ -75,10 +84,69 @@ class EfficientNetSeg(nn.Module):
                 else:
                     fill_fc_weights(fc)
             self.__setattr__(head, fc)
-            self.__setattr__(head, fc)
+
+    def _get_deconv_cfg(self, deconv_kernel, index):
+        if deconv_kernel == 4:
+            padding = 1
+            output_padding = 0
+        elif deconv_kernel == 3:
+            padding = 1
+            output_padding = 1
+        elif deconv_kernel == 2:
+            padding = 0
+            output_padding = 0
+
+        return deconv_kernel, padding, output_padding
+
+    def _make_deconv_layer(self, num_layers, num_filters, num_kernels):
+        assert num_layers == len(num_filters), \
+            'ERROR: num_deconv_layers is different len(num_deconv_filters)'
+        assert num_layers == len(num_kernels), \
+            'ERROR: num_deconv_layers is different len(num_deconv_filters)'
+
+        layers = []
+        for i in range(num_layers):
+            kernel, padding, output_padding = \
+                self._get_deconv_cfg(num_kernels[i], i)
+
+            planes = num_filters[i]
+            fc = DCN(self.inplanes, planes,
+                     kernel_size=(3, 3), stride=1,
+                     padding=1, dilation=1, deformable_groups=1)
+            # fc = nn.Conv2d(self.inplanes, planes,
+            #         kernel_size=3, stride=1,
+            #         padding=1, dilation=1, bias=False)
+            # fill_fc_weights(fc)
+            up = nn.ConvTranspose2d(
+                in_channels=planes,
+                out_channels=planes,
+                kernel_size=kernel,
+                stride=2,
+                padding=padding,
+                output_padding=output_padding,
+                bias=self.deconv_with_bias)
+            fill_up_weights(up)
+
+            layers.append(fc)
+            layers.append(nn.BatchNorm2d(planes, momentum=BN_MOMENTUM))
+            layers.append(nn.ReLU(inplace=True))
+            layers.append(up)
+            layers.append(nn.BatchNorm2d(planes, momentum=BN_MOMENTUM))
+            layers.append(nn.ReLU(inplace=True))
+            self.inplanes = planes
+
+        return nn.Sequential(*layers)
+
 
     def forward(self, x):
+
+
         x = self.model.extract_features(x)
+        #print ("post efficient {}".format(x.shape))
+        x = self.deconv_layers(x)
+        #print ("post deconv_layers {}".format(x.shape))
+        #x = torch.nn.functional.interpolate(x,size=(int(x.size(1)*2), int(x.size(1)*2)), mode='bilinear',align_corners=True)
+        #print ("post interpolation {}".format(x.shape))
         ret = {}
         for head in self.heads:
             ret[head] = self.__getattr__(head)(x)
@@ -86,6 +154,7 @@ class EfficientNetSeg(nn.Module):
 
 
 def get_pose_net(num_layers, heads, head_conv=256):
+  print (num_layers)
   model = EfficientNetSeg(num_layers, heads,
                  pretrained=True,
                  head_conv=head_conv)
