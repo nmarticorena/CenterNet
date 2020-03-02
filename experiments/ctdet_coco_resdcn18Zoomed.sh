564bf6e9b695a4769aa736f515e580b446db68cb
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2020-03-02 18:36:22 -0300

    Add training models Efficient net b3 and resdcn18zoomed

---
+++ experiments/ctdet_coco_resdcn18Zoomed.sh
@@ -0,0 +1,11 @@
+cd src
+# train
+python main.py ctdet --exp_id coco_res2dcnZoomed18 --arch res2dcnZoomed_18 --batch_size 114 --master_batch 18 --lr 5e-4 --gpus 0,1,2,3 --num_workers 16
+# test
+python test.py ctdet --exp_id coco_res2dcnZoomed18 --arch res2dcnZoomed_18 --keep_res --resume
+# flip test
+python test.py ctdet --exp_id coco_res2dcnZoomed18 --arch res2dcnZoomed_18 --keep_res --resume --flip_test
+# multi scale test
+python test.py ctdet --exp_id coco_res2dcnZoomed18 --arch res2dcnZoomed_18 --keep_res --resume --flip_test --test_scales 0.5,0.75,1,1.25,1.5
+
+cd ..
